2cd1d3a3b024cf67ac0df2fbb11634dc269eb479
Tuukka Partanen <tospartanen@gmail.com>
2019-04-04 14:14:11 +0300

    begun creating tags

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ApplicationRecord
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+    
+    def tag_list
+        self.tags.collect do |tag|
+            tag.name
+        end.join(", ")
+    end
+    
+    def tag_list=(tags.string)
+        tag_names = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+    end
+    
 end
